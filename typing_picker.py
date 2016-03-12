cd2e8ca52e0fbd428c98c5703ee1de329ac7e9fd
Bell Codo <bellcodo@gmail.com>
2016-03-11 17:50:52 -0800

    Update typing_picker.py

--- typing_picker.py
+++ typing_picker.py
@@ -6,10 +6,16 @@ url_for_list = "https://raw.githubusercontent.com/bellcodo/laughing-meme/master/
 raw_typing_options = urllib2.urlopen(url_for_list)
 list_of_options = raw_typing_options.read()
 #print "DEBUG - raw typing options is %s, %s" % (raw_typing_options, list_of_options)
-typing_options = ['abc','def', 'ghi']
+typing_options = list_of_options.split()
 
 # find a way to pick something
 typing_choice = random.choice(typing_options)
 
 # a way to tell the user what to do
-print typing_choice
+print """This is the typing program picker.
+It will pick a random typing_choice from a list.
+The list is kept here %s
+Todays typing choice is...
+
+%s
+""" % (url_for_list, typing_choice)
